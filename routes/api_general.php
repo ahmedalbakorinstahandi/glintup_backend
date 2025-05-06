5856140c7cb6e0d02a19254ec46ad1b089172a3a
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-05-06 17:36:53 +0300

    fix: Refactor notification routes to improve structure and authentication handling

--- routes/api_general.php
+++ routes/api_general.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\General;
 
 use App\Http\Controllers\Salons\SalonPermissionController;
 use App\Http\Controllers\Users\UserController;
+use App\Models\Users\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::post('general/upload-image', [ImageController::class, 'uploadImage']);
@@ -14,28 +16,29 @@ Route::prefix('general')->group(function () {
 
 
 
+    if (Auth::check()) {
+        Route::middleware(['auth:sanctum'])->group(function () {
 
-    Route::middleware(['auth:sanctum'])->group(function () {
 
+            Route::prefix('notifications')->controller(NotificationController::class)->group(function () {
+                Route::get('/', 'index');
+                Route::get('/unread-count', 'unreadCount');
+                Route::post('/{id}/read',  'readNotification');
+                Route::get('{id}', 'show');
+            });
 
+            // me
+            Route::prefix('profile')->group(function () {
+                Route::get('/', [UserController::class, 'getProfile']);
+                Route::put('/', [UserController::class, 'updateProfile']);
+            });
+
+            Route::get('/salon-permissions', [SalonPermissionController::class, 'index']);
+        });
+    } else {
         Route::prefix('notifications')->controller(NotificationController::class)->group(function () {
             Route::get('/', 'index');
             Route::get('/unread-count', 'unreadCount');
-            Route::post('/{id}/read',  'readNotification');
-            Route::get('{id}', 'show');
         });
-
-        // me
-        Route::prefix('profile')->group(function () {
-            Route::get('/', [UserController::class, 'getProfile']);
-            Route::put('/', [UserController::class, 'updateProfile']);
-        });
-
-        Route::get('/salon-permissions', [SalonPermissionController::class, 'index']);
-    });
-
-    Route::prefix('notifications')->controller(NotificationController::class)->group(function () {
-        Route::get('/', 'index');
-        Route::get('/unread-count', 'unreadCount');
-    });
+    }
 });
