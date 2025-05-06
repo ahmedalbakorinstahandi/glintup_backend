7d44e75928f70ac63a725b354fd388088d25dd3b
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-05-06 19:33:45 +0300

    fix: Reorganize route structure for guests and customer authentication

--- routes/api_customer.php
+++ routes/api_customer.php
@@ -15,16 +15,7 @@ use App\Http\Controllers\Users\WalletTransactionController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix('customer')->group(function () {
-
-    Route::prefix('auth')->group(function () {
-        Route::post('/login', [UserAuthController::class, 'login']);
-        Route::post('/register', [UserAuthController::class, 'register']);
-        Route::post('/verify-code', [UserAuthController::class, 'verifyCode']);
-        Route::post('/forgot-password', [UserAuthController::class, 'forgotPassword']);
-        Route::post('/reset-password', [UserAuthController::class, 'resetPassword'])->middleware('auth:sanctum');
-        Route::post('/logout', [UserAuthController::class, 'logout'])->middleware('auth:sanctum');
-    });
+Route::prefix('guests')->group(function () {
 
     Route::prefix('home')->group(function () {
         Route::get('/search', [SalonController::class, 'index']);
@@ -49,6 +40,19 @@ Route::prefix('customer')->group(function () {
         Route::get('/', 'index');
         Route::get('{id}', 'show');
     });
+});
+
+Route::prefix('customer')->group(function () {
+
+    Route::prefix('auth')->group(function () {
+        Route::post('/login', [UserAuthController::class, 'login']);
+        Route::post('/register', [UserAuthController::class, 'register']);
+        Route::post('/verify-code', [UserAuthController::class, 'verifyCode']);
+        Route::post('/forgot-password', [UserAuthController::class, 'forgotPassword']);
+        Route::post('/reset-password', [UserAuthController::class, 'resetPassword'])->middleware('auth:sanctum');
+        Route::post('/logout', [UserAuthController::class, 'logout'])->middleware('auth:sanctum');
+    });
+
 
     Route::middleware('auth:sanctum')->group(function () {
         // Booking
@@ -59,6 +63,30 @@ Route::prefix('customer')->group(function () {
             Route::post('/', 'createFromUser');
             Route::post('{id}/reschedule', 'rescheduleBooking');
             Route::post('{id}/cancel', 'cancelBooking');
+        });
+
+        Route::prefix('home')->group(function () {
+            Route::get('/search', [SalonController::class, 'index']);
+            Route::get('/data', [UserController::class, 'homeData']);
+        });
+
+
+        // salons
+        Route::prefix('salons')->controller(SalonController::class)->group(function () {
+            Route::get('/', 'index');
+            Route::get('{id}', 'show');
+        });
+
+
+        Route::prefix('groups')->controller(GroupController::class)->group(function () {
+            Route::get('/', 'index');
+            Route::get('{id}', 'show');
+        });
+
+        // services
+        Route::prefix('services')->controller(ServiceController::class)->group(function () {
+            Route::get('/', 'index');
+            Route::get('{id}', 'show');
         });
 
 
