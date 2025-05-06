f656002b38f2f9bacc0bdebbfdb8641b93baa170
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-05-06 17:38:32 +0300

    fix: Simplify notification routes by removing unnecessary authentication checks

--- routes/api_general.php
+++ routes/api_general.php
@@ -4,41 +4,39 @@ namespace App\Http\Controllers\General;
 
 use App\Http\Controllers\Salons\SalonPermissionController;
 use App\Http\Controllers\Users\UserController;
-use App\Models\Users\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::post('general/upload-image', [ImageController::class, 'uploadImage']);
 
 
+Route::prefix('guests')->group(function () {
+    Route::prefix('notifications')->controller(NotificationController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('/unread-count', 'unreadCount');
+    });
+});
 Route::prefix('general')->group(function () {
 
 
 
 
-    if (Auth::check()) {
-        Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::middleware(['auth:sanctum'])->group(function () {
 
 
-            Route::prefix('notifications')->controller(NotificationController::class)->group(function () {
-                Route::get('/', 'index');
-                Route::get('/unread-count', 'unreadCount');
-                Route::post('/{id}/read',  'readNotification');
-                Route::get('{id}', 'show');
-            });
-
-            // me
-            Route::prefix('profile')->group(function () {
-                Route::get('/', [UserController::class, 'getProfile']);
-                Route::put('/', [UserController::class, 'updateProfile']);
-            });
-
-            Route::get('/salon-permissions', [SalonPermissionController::class, 'index']);
-        });
-    } else {
         Route::prefix('notifications')->controller(NotificationController::class)->group(function () {
             Route::get('/', 'index');
             Route::get('/unread-count', 'unreadCount');
+            Route::post('/{id}/read',  'readNotification');
+            Route::get('{id}', 'show');
         });
-    }
+
+        // me
+        Route::prefix('profile')->group(function () {
+            Route::get('/', [UserController::class, 'getProfile']);
+            Route::put('/', [UserController::class, 'updateProfile']);
+        });
+
+        Route::get('/salon-permissions', [SalonPermissionController::class, 'index']);
+    });
 });
