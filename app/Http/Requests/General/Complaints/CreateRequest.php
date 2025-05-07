061f803a3558d39e91db00844bdf9f06a60cca78
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-05-07 09:54:57 +0300

    fix: Correct phone_number validation rule in CreateRequest to require when hide_identity is false

--- app/Http/Requests/General/Complaints/CreateRequest.php
+++ app/Http/Requests/General/Complaints/CreateRequest.php
@@ -11,7 +11,7 @@ class CreateRequest extends BaseFormRequest
         return [
             'hide_identity' => 'required|boolean',
             'content'       => 'required|string|max:1000',
-            'phone_number'  => 'required_if:hide_identity,true|nullable|string|max:15',
+            'phone_number'  => 'required_if:hide_identity,false|nullable|string|max:15',
         ];
     }
 }
