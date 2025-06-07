5a45755b5e682320c9ec6583d6cda54ac4342487
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-06-07 22:25:01 +0300

    feat: Refactor data.json to replace email list with multilingual content and enhance ActivityLog model for translatable descriptions

--- app/Models/General/ActivityLog.php
+++ app/Models/General/ActivityLog.php
@@ -3,16 +3,24 @@
 namespace App\Models\General;
 
 use App\Models\Users\User;
+use App\Services\LanguageService;
+use App\Traits\LanguageTrait;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Translatable\HasTranslations;
 
 class ActivityLog extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasTranslations, LanguageTrait;
 
     protected $fillable = [
         'user_id',
         'action',
+        'description',
+    ];
+
+    public $translatable = [
         'description',
     ];
 
@@ -26,4 +34,17 @@ class ActivityLog extends Model
     {
         return $this->belongsTo(User::class)->withTrashed();
     }
+
+
+    protected function description(): Attribute
+    {
+        $multi = LanguageService::getMultiLanguage();
+
+        return Attribute::make(
+            get: fn(string $value) => $multi ? $this->getAllTranslations('description') : $value,
+        );
+    }
+
+
+
 }
