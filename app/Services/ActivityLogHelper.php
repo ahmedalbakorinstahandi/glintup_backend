5849bbb9f4f2b2629e8d2dc0e1e7efe58888c4a0
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-07-22 17:48:40 +0300

    refactor: Enhance createActivityLog method to include logging of activity creation for better traceability

--- app/Services/ActivityLogHelper.php
+++ app/Services/ActivityLogHelper.php
@@ -3,17 +3,31 @@
 namespace App\Services;
 
 use App\Models\General\ActivityLog;
+use Illuminate\Support\Facades\Log;
 
 class ActivityLogHelper
 {
     public static function createActivityLog($user_id, $action, $description, $activityable_type = null, $activityable_id = null)
     {
-        ActivityLog::create([
+        Log::info('createActivityLog', [
             'user_id' => $user_id,
             'action' => $action,
             'description' => $description,
             'activityable_type' => $activityable_type,
             'activityable_id' => $activityable_id,
         ]);
+        $activityLog = ActivityLog::create([
+            'user_id' => $user_id,
+            'action' => $action,
+            'description' => $description,
+            'activityable_type' => $activityable_type,
+            'activityable_id' => $activityable_id,
+        ]);
+
+        Log::info('activityLog', [
+            'activityLog' => $activityLog,
+        ]);
+
+        return $activityLog;
     }
 }
