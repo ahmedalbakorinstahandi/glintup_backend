9dc5642d5e95c62cb1cccc2cd5b7d18c34f8cd35
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-07-26 13:34:52 +0300

    refactor: Comment out conditional retrieval logic in groupServices method to improve code clarity

--- app/Models/Services/Group.php
+++ app/Models/Services/Group.php
@@ -61,9 +61,9 @@ class Group extends Model
         }
 
 
-        if ($this->key == 'new') {
-            return $this->groupServices()->where('salon_id', $salon_id)->orderBy('order', 'desc')->limit(10)->get();
-        }
+        // if ($this->key == 'new') {
+        //     return $this->groupServices()->where('salon_id', $salon_id)->orderBy('order', 'desc')->limit(10)->get();
+        // }
 
         // if ($this->key == 'popular') {
         //     return $this->groupServices()->where('salon_id', $salon_id)->orderBy('order', 'desc')->limit(10)->get();
