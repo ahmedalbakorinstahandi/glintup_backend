e5b764f9cb7e3bc4b90e43c84e26eb1704ebd70d
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-07-26 14:20:22 +0300

    refactor: Restore conditional retrieval logic in groupServices method to improve service management based on salon_id

--- app/Models/Services/Group.php
+++ app/Models/Services/Group.php
@@ -61,9 +61,9 @@ class Group extends Model
         }
 
 
-        // if ($this->key == 'new') {
-        //     return $this->groupServices()->where('salon_id', $salon_id)->orderBy('order', 'desc')->limit(10)->get();
-        // }
+        if ($this->key == 'new') {
+            return $this->hasMany(GroupService::class)->where('salon_id', $salon_id)->orderBy('created_at', 'desc')->limit(10);
+        }
 
         // if ($this->key == 'popular') {
         //     return $this->groupServices()->where('salon_id', $salon_id)->orderBy('order', 'desc')->limit(10)->get();
